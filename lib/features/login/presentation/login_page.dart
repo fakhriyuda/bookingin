de9586d30d6308ed2732af79a97a94ff6bd81b1c
Yuda <codeyuda@gmail.com>
2023-11-30 10:23:22 +0700

    overflow when horizontal login page

--- lib/features/login/presentation/login_page.dart
+++ lib/features/login/presentation/login_page.dart
@@ -17,86 +17,90 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(24.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "images/logo_bookingin.png",
-                height: 200,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Username"),
-                onChanged: (value) {
-                  userCtrl.text = value;
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please fill the username";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  passCtrl.text = value;
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please fill the password";
-                  } else {
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                    labelText: "Password",
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          debugPrint("Yud $seePassword");
-                          setState(() {
-                            seePassword = !seePassword;
-                          });
-                        },
-                        icon: Icon(seePassword
-                            ? Icons.remove_red_eye_outlined
-                            : Icons.lock))),
-                obscureText: seePassword ? false : true,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  debugPrint(userCtrl.text);
-                  debugPrint(passCtrl.text);
-                  if (formKey.currentState!.validate()) {
-                    if (userCtrl.text == "user" && passCtrl.text == "pass") {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
-                        ),
-                      );
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Image.asset(
+                    "images/logo_bookingin.png",
+                    width: MediaQuery.of(context).size.width/2,
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Username"),
+                  onChanged: (value) {
+                    userCtrl.text = value;
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please fill the username";
                     } else {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content: Text("Wrong username / password")));
+                      return null;
                     }
-                  } else {
-                    debugPrint("Masih error nih");
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: ColorB.gold,
-                    foregroundColor: Colors.white,
-                    minimumSize: const Size(double.infinity, 36)),
-                child: const Text('Login'),
-              )
-            ],
+                  },
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    passCtrl.text = value;
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please fill the password";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            debugPrint("Yud $seePassword");
+                            setState(() {
+                              seePassword = !seePassword;
+                            });
+                          },
+                          icon: Icon(seePassword
+                              ? Icons.remove_red_eye_outlined
+                              : Icons.lock))),
+                  obscureText: seePassword ? false : true,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint(userCtrl.text);
+                    debugPrint(passCtrl.text);
+                    if (formKey.currentState!.validate()) {
+                      if (userCtrl.text == "user" && passCtrl.text == "pass") {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomePage(),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text("Wrong username / password")));
+                      }
+                    } else {
+                      debugPrint("Masih error nih");
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorB.gold,
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(double.infinity, 36)),
+                  child: const Text('Login'),
+                )
+              ],
+            ),
           ),
         ),
       ),
